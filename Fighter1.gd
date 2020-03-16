1aed45e1dfddbdf9ba9a341bcf097b0bfaebd257
Jonte-J <Jonte-J@users.noreply.github.com>
2020-03-16 15:39:20 +0100

    First commit

---
+++ Fighter1.gd
@@ -0,0 +1,30 @@
+extends KinematicBody2D
+
+onready var animation = get_node("../../test/KinematicBody2D/Sprite/AnimationPlayer") 
+onready var coll = get_node("../../test/KinematicBody2D/CollisionShape2D:shape")
+const UP = Vector2(0, -1)
+var motion = Vector2()
+func _ready():
+    print(self.get_path())
+func _on_AnimationPlayer_ready():
+	pass
+func _physics_process(delta):
+	motion.y += 10
+	
+	if Input.is_action_pressed("ui_right"):
+		motion.x = 130
+		animation.play("walkR") 
+	elif Input.is_action_pressed("ui_left"):
+		motion.x = -130
+		animation.play("walkL")
+	else:
+		motion.x = 0
+		
+	
+	if is_on_floor():
+		if Input.is_action_just_pressed("ui_up"):
+			motion.y = -285
+	
+	motion = move_and_slide(motion, UP)
+	
+	pass
---
+++ Fighter2.gd
@@ -0,0 +1,23 @@
+extends KinematicBody2D
+
+
+const UP = Vector2(0, -1)
+var motion = Vector2()
+
+func _physics_process(delta):
+	motion.y += 10
+	
+	if Input.is_action_pressed("ui_right2"):
+		motion.x = 130
+	elif Input.is_action_pressed("ui_left2"):
+		motion.x = -130
+	else:
+		motion.x = 0
+	
+	if is_on_floor():
+		if Input.is_action_just_pressed("ui_up2"):
+			motion.y = -285
+	
+	motion = move_and_slide(motion, UP)
+	
+	pass
